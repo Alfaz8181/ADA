645a3d1104793de31c6f21e99854ad2c0ecba8b8
ALFAZ KALADAGI <kaltex.alfaz@gmail.com>
2025-05-20 10:25:04 +0530

    Add files via upload

---
+++ BruteForceSearch.java
@@ -0,0 +1,13 @@
+public class BruteForceSearch {
+    public static int search(int[] arr, int target) {
+    for (int i = 0; i < arr.length; i++) {
+    if (arr[i] == target) return i;
+    }
+    return -1;
+    }
+    public static void main(String[] args) {
+    int[] arr = {4, 2, 7, 1, 3};
+    System.out.println(search(arr, 7)); // Output: 2
+    }
+   } 
+   
